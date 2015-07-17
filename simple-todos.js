55e14e3838b99379299a6f738ead9e52fa2de59a
xiang <bylixiang@gmail.com>
2015-07-17 01:04:42 -0700

    meteor tutorial

---
+++ simple-todos.js
@@ -0,0 +1,59 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+
+  Template.body.helpers({
+    tasks: function() {
+      if(Session.get("hideCompleted")){
+        return Tasks.find({checked: {$ne: true}}, {sorted: {createdAt: -1}});
+      }else{
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function(){
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function(){
+      return Tasks.find({checked: {$ne: true}}).count();
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function(event){
+      //prevent default browser form submit
+      event.preventDefault();
+
+      //Get value from form element
+      var text = event.target.text.value;
+
+      //Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      //Clear form
+      event.target.text.value = "";
+    },
+    "change .hide-completed": function (event){
+      Session.set("hideCompleted", event.target.checked);
+    }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function(){
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function(){
+      Tasks.remove(this._id);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
